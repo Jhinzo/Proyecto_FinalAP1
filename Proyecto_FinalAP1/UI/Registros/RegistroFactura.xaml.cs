9a9e14543f891a76cea8efcd0e79e1afcc6eae37
Bleimy De Jesus Caceres <bleimy_dejesus@ucne.edu.do>
2020-08-07 21:17:44 -0400

    se creo el registro factura

---
+++ Proyecto_FinalAP1/UI/Registros/RegistroFactura.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Proyecto_FinalAP1.UI.Registros
+{
+    /// <summary>
+    /// Interaction logic for RegistroFactura.xaml
+    /// </summary>
+    public partial class RegistroFactura : Window
+    {
+        public RegistroFactura()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
